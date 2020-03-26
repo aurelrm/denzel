1a3b5a1147559d2467754ada24d3c3c70d4f0948
aurelien <aurelienrame@bbox.fr>
2020-03-26 13:12:37 +0100

    feat(movies): get a random movie

---
+++ server/ConnectAndApi.js
@@ -0,0 +1,99 @@
+const Express = require("express");
+const BodyParser = require("body-parser");
+var MongoClient = require("mongodb").MongoClient;
+const graphqlHTTP = require('express-graphql');
+
+const {GraphQLSchema} = require('graphql');
+const CONNECTION_URL = "mongodb+srv://aurelrm:aurelrm@thecluster-tygpr.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const {queryType} = require('./query.js')
+const schema = new GraphQLSchema({ query: queryType });
+
+const DATABASE_NAME = "movies";
+
+var app = Express();
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+app.listen(3000, () => {
+    
+    MongoClient.connect(CONNECTION_URL,{ useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            
+            console.log(error) ;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("denzel");
+        
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.get("/movies/populate", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+app.get("/movie", (request, response) => {
+    collection.findOne({"metascore" : {"$gt" : 70}}, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+  });
+
+app.get("/movies/search", (request, response) => {
+    var req=request.query;
+    var metascore;
+    if(req.metascore == null)
+    {
+        metascore=0;
+    }else{metascore=Number(req.metascore);}
+    
+    var limit;
+    if(req.limit == null)
+    {
+        limit = 5
+    }
+    else{limit=Number(req.limit)}
+   
+    
+    collection.find({ "metascore" : {"$gt":metascore}}).sort({"metascore":-1}).limit(limit).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get("/movie/:id", (request, response) => {
+    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);  
+    });
+});
+
+app.post("/movie/:id", (request, response) => {
+    req=request.body;
+    collection.updateOne({id:request.params.id},{$set:{date:req.date,review:req.review}},(error, result) => {           
+        if(error) {
+            return response.status(500).send(error);
+        }           
+        response.send(result)          
+    });
+});
+
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+
+}));
